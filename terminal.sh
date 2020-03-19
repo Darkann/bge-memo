4101d79bbcc6dece047fb69639219baa720700f2
Darkann <60514789+Darkann@users.noreply.github.com>
2020-03-19 10:27:08 +0100

    nouvelle version rappel

--- terminal.sh
+++ terminal.sh
@@ -9,5 +9,7 @@ git commit -m "<message à attacher au commit>"                    # sauvegardé
 git status                                                        # affiche l'etat de mon projet a cet instant
 git log                                                           # affiche l'historique des commit
 git add.                                                          # rajouter tout le dossier d'un coup
-git restore <nom du fichier / dossier>                            #remet le contenu du fichier a l'état du dernier commit
-
+git restore <nom du fichier / dossier>                            # remet le contenu du fichier a l'état du dernier commit
+git checkout <nom dune branche ou hash du commit>                 # deplace la tete de lecture (head) sur la branche ou sur la version choisi
+git branch <nom de la branche>                                    # créé une branche en copiant la branche actuelle
+git branch                                                        # liste les branches existante
