7499db49b38e1c18adf36ec5b1ceb449cefe9241
Darkann <60514789+Darkann@users.noreply.github.com>
2020-03-19 09:38:04 +0100

    fichier de rappel

---
+++ terminal.sh
@@ -0,0 +1,11 @@
+#divers
+cd <chemin du dossier>                                            # se deplacer
+node <nom du fichier>                                             # lancer un scipt node
+
+#git
+git init                                                          # initialiser un projet git
+git add <nom du fichier/dossier>                                  # ajouter les modifications au prochain commit
+git commit -m "<message à attacher au commit>"                    # sauvegardé les modifications precedement rajoutées
+git status                                                        # affiche l'etat de mon projet a cet instant
+git log                                                           # affiche l'historique des commit
+git add.                                                          # rajouter tout le dossier d'un coup
